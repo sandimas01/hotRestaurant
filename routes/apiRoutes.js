99c4a9bab3084c3727db0730356b256a2b5968bd
Mustafa Farah <mustafa.farah@outlook.com>
2018-05-07 19:37:22 -0500

    apiRoute

---
+++ routes/apiRoutes.js
@@ -0,0 +1,25 @@
+var tableData = require("../data/tableData");
+var waitListData = require("../data/waitinglistData");
+
+// ROUTING
+module.exports = function(app) {
+  app.get("/api/tables", function(req, res) {
+    res.json(tableData);
+  });
+
+  app.get("/api/waitlist", function(req, res) {
+    res.json(waitListData);
+  });
+
+  app.post("/api/tables", function(req, res) {
+    if (tableData.length < 5) {
+      tableData.push(req.body);
+      res.json(true);
+    }
+    else {
+      waitListData.push(req.body);
+      res.json(false);
+    }
+  });
+
+};
