07bd5cbfe567d9d6e1f85ac4e202f8f1cc95457e
Mustafa Farah <mustafa.farah@outlook.com>
2018-05-07 19:19:20 -0500

    adding table arrays

---
+++ tableDate.js
@@ -0,0 +1,11 @@
+var tableArray = [
+    {
+      customerName: "Parker",
+      customerEmail: "ahmed@matt.com",
+      customerID: "idkbutmattisbetter23",
+      phoneNumber: "000-000-0000"
+    }
+  ];
+ 
+  module.exports = tableArray;
+  
---
+++ waitingList.js
@@ -0,0 +1,11 @@
+var waitingArray = [
+    {
+      customerName: "Team6",
+      customerEmail: "team6@example.com",
+      phoneNumber: "000-000-0000",
+      customerID: "team6isCool"
+    }
+  ];
+  
+  module.exports = waitingArray;
+  
